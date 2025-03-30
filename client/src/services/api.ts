f15ed977aba0bd22e866b547650826b05f00c7a6
Patrick Amsler <patrick.amsler@abraxas.ch>
2025-03-30 16:27:27 +0200

    .env added

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:5000';
+const API_URL = process.env.REACT_APP_API_URL || (() => {
+  throw new Error('REACT_APP_API_URL environment variable is not set');
+})();
 
 export const uploadFile = async (file: File) => {
   const formData = new FormData();
