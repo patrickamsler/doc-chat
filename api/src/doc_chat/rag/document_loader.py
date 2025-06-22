8faa533d4d9123f417ac5c4b8d106ba6b16203d5
Patrick Amsler <amslerpatrick@bluemail.ch>
2025-06-22 18:17:39 +0200

    predict single passage answers

--- api/src/doc_chat/rag/document_loader.py
+++ api/src/doc_chat/rag/document_loader.py
@@ -1,13 +1,17 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
+
 class DocumentLoader:
     def __init__(self, file_path: str):
         self._file_path = file_path
 
-    def load_and_split(self) -> tuple[list, list]:
+    def load_and_split(self,
+          chuck_size: int = 1000,
+          check_overlap=150) -> tuple[list, list]:
         loader = PyPDFLoader(self._file_path)
         documents = loader.load()
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=chuck_size,
+                                                       chunk_overlap=check_overlap)
         splits = text_splitter.split_documents(documents)
         return documents, splits
