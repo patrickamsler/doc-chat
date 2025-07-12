cfa85a33c665417f06858c747c1c24a60bacea7f
Patrick Amsler <amslerpatrick@bluemail.ch>
2025-07-12 19:12:03 +0200

    - app data path for development changed - fix error in model id

--- api/src/doc_chat/llm.py
+++ api/src/doc_chat/llm.py
@@ -3,7 +3,7 @@ from langchain_core.language_models import BaseChatModel
 
 def create_llm() -> BaseChatModel:
     return ChatOpenAI(
-        model="gpt-4o-mini-2024-07-18'",
+        model="gpt-4o-mini-2024-07-18",
         temperature=0.0,
         top_p=1.0,
         max_tokens=2048
